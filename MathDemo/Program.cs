2c2e5a6135aff1b1d08f8c5318c547f5d66e95a7
Francis Bulu <Francis.Bulu@student.intecbrussel.be>
2020-10-30 10:56:03 +0100

    Add project files.

---
+++ MathDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = 548.65;
+            Console.WriteLine($"Floor value of {num1} is {Math.Floor(num1)}");
+            Console.WriteLine($"Cell value of {num1} is {Math.Ceiling(num1)}");
+            Console.WriteLine($"Round {num1} to {Math.Round(num1)}");
+            char kar = 'a';
+            Console.WriteLine((int)kar);
+           
+        }
+    }
+}
